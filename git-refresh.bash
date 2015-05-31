826e2c1b7ed851cc03d6867e6f8f863cb0b8ce87
Iuri Guilherme <iuri@piratas.xyz>
2015-05-31 03:58:39 +0200

    Adicionados comentarios

--- git-refresh.bash
+++ git-refresh.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
+## Este script serve para atualizar todos reposit√rios git bem configurados
+## O que n√o tiver configruado com `git branch --set-upstream` n√o vai ser atualizado
 
 for REPO in ${HOME}/git/*
 do
-        cd ${REPO}
-        /usr/bin/git pull
+	if [ -d "${REPO}" ]
+	then
+	        pushd "${REPO}"
+	        /usr/bin/git pull
+		popd
+	fi
 done
 
