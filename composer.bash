80410bb19546b85d63563ec75e5d9229655af6c2
Iuri Guilherme <iuri@piratas.xyz>
2015-05-31 03:57:35 +0200

    Adicionado comentários

--- composer.bash
+++ composer.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
+## Este script serve para rodar o composer.phar sem adicionar no PATH
+## Utiliza-se composer.bash ao invés de composer.phar em qualquer diretóri
+
 COMPOSER="${HOME}/lib/composer.phar"
 PHP="/usr/bin/php"
 ARGS="--working-dir=`pwd`"
 
-${PHP} ${COMPOSER} ${ARGS} ${@}
+${PHP} ${COMPOSER} ${ARGS} ${@} # Argumentos da linha de comando
