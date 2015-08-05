3c86e28fe224d9bdb06a5f15c7bd3f2f8b493017
Iuri Guilherme <iuri@piratas.xyz>
2015-08-05 08:16:43 +0200

    Removi o pid quando o comando de stop não funciona

--- mg-stop.bash
+++ mg-stop.bash
@@ -3,6 +3,10 @@
 
 MG_P="/etc/init.d/mg-paster"
 MG_CW="/etc/init.d/mg-celery-worker"
+MG_P_PID="/var/run/mediagoblin/mg-paster.pid"
+MG_CW_PID="/var/run/mediagoblin/mg-celery-worker.pid"
 
-sudo ${MG_P} stop
-sudo ${MG_CW} stop
+
+sudo ${MG_P} stop || sudo rm ${MG_P_PID}
+sudo ${MG_CW} stop || sudo rm ${MG_CW_PID}
+
