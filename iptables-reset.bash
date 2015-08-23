99303bece4c2b05238cb2923c436ee62679686b9
Desobediente Civil <desci@riseup.net>
2015-08-23 20:35:07 -0300

    Atualizando script do iptables

--- iptables-reset.bash
+++ iptables-reset.bash
@@ -7,27 +7,29 @@
 
    ##
    ## set default policies to let everything in
-   ip6tables --policy INPUT   ACCEPT;
-   ip6tables --policy OUTPUT  ACCEPT;
-   ip6tables --policy FORWARD ACCEPT;
+
+sudo ip6tables --policy INPUT ACCEPT
+sudo ip6tables --policy OUTPUT ACCEPT
+sudo ip6tables --policy FORWARD ACCEPT
 
    ##
    ## start fresh
-   ip6tables -Z; # zero counters
-   ip6tables -F; # flush (delete) rules
-   ip6tables -X; # delete all extra chains
+
+sudo ip6tables -Z # zero counters
+sudo ip6tables -F # flush (delete) rules
+sudo ip6tables -X # delete all extra chains
 
 # IPv4
 
    ## 
    ## set default policies to let everything in
-   iptables --policy INPUT   ACCEPT;
-   iptables --policy OUTPUT  ACCEPT;
-   iptables --policy FORWARD ACCEPT;
+sudo iptables --policy INPUT ACCEPT
+sudo iptables --policy OUTPUT ACCEPT
+sudo iptables --policy FORWARD ACCEPT
 
    ##
    ## start fresh
-   iptables -Z; # zero counters
-   iptables -F; # flush (delete) rules
-   iptables -X; # delete all extra chains
+sudo iptables -Z # zero counters
+sudo iptables -F # flush (delete) rules
+sudo iptables -X # delete all extra chains
 
