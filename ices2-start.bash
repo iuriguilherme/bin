1ccc9f7eb5b401f6e104e9c29b79e95abea3fdc9
Desobediente Civil <desci@riseup.net>
2016-02-20 17:27:52 -0200

    Mais alguns ajustes

--- ices2-start.bash
+++ ices2-start.bash
@@ -7,7 +7,7 @@ ICES2="/usr/bin/ices2"
 if  [ ! -z $1 ]
 then
 	CANAL="${1}"
-	ICES2="`whereis ices2-${CANAL}`"
+	ICES2="`whereis ices2-${CANAL} | awk '{ print $2}'`"
 	${ICES2} ${HOME}/ices2.d/${CANAL}-playlist.xml
 else
 	echo "E o parâmetro?"
