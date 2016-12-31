dbd932f87bc275b46ac8a80dc9be4908623ce628
Desobediente Civil <desci@riseup.net>
2016-12-31 16:40:44 +0000

    Adicionando link do tutorial

--- nolog.bash
+++ nolog.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
-## Apaga essa porra de log
+##
+## Apaga logs do sistema, em desobediência ao artigo 15 do marco civil
+## Mais informações: https://iikb.org/tutorial/Apagar_logs_no_Linux/
+##
+## Domínio Público igreja insurgente Kopimista Brasileira 2016
+##
 
+## Trabalho em progresso, quem tiver mais caminhos de arquivos de log padrão pra providenciar, sinta-se à vontade.
 LOGS=( \
 "/var/log/nginx/access.log" \
 "/var/log/nginx/error.log" \
