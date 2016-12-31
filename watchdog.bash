8bc65952aa44701f598e774aeb6ca61ed1143a2f
Desobediente Civil <desci@riseup.net>
2016-12-31 14:40:57 +0000

    Adicionando script pra monitorar serviços

---
+++ watchdog.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+##
+## Monitora serviços para garantir que estão online
+## Exemplo de uso em https://iikb.org/forum/Zeronet/
+## Adicionar uma linha semelhante na crontab para rodar de 5 em 5 minutos:
+## */5 /home/user/bin/watchdog.bash
+##
+## Domínio Público igreja insurgente Kopimista Brasileira 2016
+##
+
+SERVICOS_ICECAST=( \
+"icecast2" \
+"ices2-akasha" \
+"ices2-balaio" \
+"ices2-cymatic" \
+"ices2-lapirata" \
+"ices2-theerm" \
+)
+
+SERVICOS_WEB=( \
+"nginx" \
+"zeronet" \
+)
+
+## Comentar serviços que não estejam no servidor é opcional para diminuir o spam de erros nos logs do sistema
+SERVICOS=( \
+"${SERVICOS_ICECAST[@]}" \
+"${SERVICOS_WEB[@]}" \
+)
+
+if [ "$(whoami)" == 'root' ]
+then
+	for SERVICO in "${SERVICOS[@]}"
+	do
+		/bin/systemctl is-active "${SERVICO}".service || /bin/systemctl start "${SERVICO}".service
+	done
+else
+	echo "Script de gente grande é só pra super vacas."
+fi
+
