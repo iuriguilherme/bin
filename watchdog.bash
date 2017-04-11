e1c236c78592b4e2a6aa3099e69390efdfaa0e68
Desobediente Civil <desci@riseup.net>
2017-04-11 00:34:12 -0300

    Adicionando criptomoedas

--- watchdog.bash
+++ watchdog.bash
@@ -5,7 +5,8 @@
 ## Adicionar uma linha semelhante na crontab para rodar de 5 em 5 minutos:
 ## */5 * * * * /home/user/bin/watchdog.bash
 ##
-## Domínio Público igreja insurgente Kopimista Brasileira 2016
+## Domínio Público igreja insurgente Kopimista Brasileira 2016-2017
+## Use por conta e risco. Em caso de cagada, o problema é teu.
 ##
 
 SERVICOS_ICECAST=( \
@@ -27,6 +28,15 @@ SERVICOS_WEB=( \
 "apoio" \
 "zeronet" \
 "ipfs" \
+"tor" \
+)
+
+SERVICOS_CRYPTO=( \
+"bfgminer" \
+"cgminer" \
+"pivx" \
+"libraryd" \
+"florincoin" \
 )
 
 ## Comentar serviços que não estejam no servidor é opcional para diminuir o spam de erros nos logs do sistema
@@ -34,6 +44,7 @@ SERVICOS=( \
 "${SERVICOS_ICECAST[@]}" \
 "${SERVICOS_MEDIAGOBLIN[@]}" \
 "${SERVICOS_WEB[@]}" \
+"${SERVICOS_CRYPTO[@]}" \
 )
 
 if [ "$(whoami)" == 'root' ]
