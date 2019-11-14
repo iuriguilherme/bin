c3d8b53dd81bf51eee15a3029237ff12efec0a50
Iuri Guilherme <iuri@fabricadofuturo.com>
2019-11-14 10:22:16 -0300

    Anos de estudo

--- dotfiles.bash
+++ dotfiles.bash
@@ -1,24 +1,29 @@
 #!/bin/bash
 ## Arquivos de configuração para ${HOME}
 
+ARQUIVO_BASHRC="${HOME}/.bashrc"
+ARQUIVO_BASH_ALIASES="${HOME}/.bash_aliases"
+ARQUIVO_VIMRC="${HOME}/.vimrc"
+ARQUIVO_XINITRC="${HOME}/.xinitrc"
+
 ## Adiciona aliases e functions do bash
 ## Não é bom se acostumar com comandos não padrões, porque num computador novo ou no computador das outras pessoas não vai ter essas coisas. Decore os comandos padrão!
-echo 'if [ -f ~/.bash_aliases ]' 1>> ~/.bashrc
-echo 'then' 1>> ~/.bashrc
-echo -e "\t"'. ~/.bash_aliases' 1>> ~/.bashrc
-echo 'fi' 1>> ~/.bashrc
-echo 'function rmv {' 1>> ~/.bash_aliases
-echo -e "\t"'rsync -avvvhP --remove-source-files "${@}" && find "${@}" -type d -empty -delete' 1>> ~/.bash_aliases
-echo '}' 1>> ~/.bash_aliases
-echo 'function rcp {' 1>> ~/.bash_aliases
-echo -e "\t"'rsync -avvvhP "${@}" && find "${@}" -type d -empty -delete' 1>> ~/.bash_aliases
-echo '}' 1>> ~/.bash_aliases
+echo "if [ -f ${ARQUIVO_BASH_ALIASES} ]" 1>> ${ARQUIVO_BASHRC}
+echo 'then' 1>> ${ARQUIVO_BASHRC}
+echo -e "\t"'. ${ARQUIVO_BASH_ALIASES}' 1>> ${ARQUIVO_BASHRC}
+echo 'fi' 1>> ${ARQUIVO_BASHRC}
+echo 'function rmv {' 1>> ${ARQUIVO_BASH_ALIASES}
+echo -e "\t"'rsync -avvhSP --remove-source-files "${@}" && find "${@}" -type d -empty -delete' 1>> ${ARQUIVO_BASH_ALIASES}
+echo '}' 1>> ${ARQUIVO_BASH_ALIASES}
+echo 'function rcp {' 1>> ${ARQUIVO_BASH_ALIASES}
+echo -e "\t"'rsync -avvhSP "${@}" && find "${@}" -type d -empty -delete' 1>> ${ARQUIVO_BASH_ALIASES}
+echo '}' 1>> ${ARQUIVO_BASH_ALIASES}
 
 ## Vim
-echo 'syntax on' 1>> ~/.vimrc
-echo 'set number' 1>> ~/.vimrc
-echo 'set tabstop=8 softtabstop=0 expandtab shiftwidth=2 smarttab' 1>> ~/.vimrc
+echo 'syntax on' 1>> ${ARQUIVO_VIMRC}
+echo 'set number' 1>> ${ARQUIVO_VIMRC}
+echo 'set tabstop=4 softtabstop=0 expandtab shiftwidth=2 smarttab' 1>> ${ARQUIVO_VIMRC}
 
 ## X11
-echo 'setxkbmap -layout "us,br" -option "grp:lwin_toggle"' 1>> ~/.xinitrc
+echo 'setxkbmap -layout "us,br" -option "grp:lwin_toggle"' 1>> ${ARQUIVO_XINITRC}
 
