840cf3dccd040cc2a96bdbbfeffe069a9b8d8b92
Iuri Guilherme <iuri@fabricadofuturo.com>
2020-01-10 15:51:13 -0300

    What is my ip via OpenDNS

--- external-ip.bash
+++ external-ip.bash
@@ -1,7 +1,18 @@
 #!/bin/bash
+## Writes external ip into a html file
+
+PUBLIC_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 INDEX_FILE="/var/www/html/ip/index.html"
-PUBLIC_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+if [ ! -z $1 ]
+then
+  INDEX_FILE="${1}"
+fi
+if [ ! -f ${INDEX_FILE} ]
+then
+  mkdir -p $(dirname ${INDEX_FILE})
+  touch ${INDEX_FILE}
+fi
 
 echo '<!DOCTYPE HTML>' 1> ${INDEX_FILE}
 echo '<html>' 1>> ${INDEX_FILE}
