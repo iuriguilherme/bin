2a5422812f376e632b4d03f41ebfea814ef63049
Iuri Guilherme <iuri@fabricadofuturo.com>
2020-01-17 15:12:47 -0300

    Facepalm

--- backup.bash
+++ backup.bash
@@ -6,7 +6,7 @@ then
   SOURCE_DIR="$(basename $1)"
   DEST_PATH="$(dirname $2)"
   DEST_FILE="$(basename $2).tar.xz"
-  XZ_OPT=-e9 ionice c 2 -n 7 nice -n 19 tar -cJhvpnS -C "${SOURCE_PATH}" "${SOURCE_DIR}" -C "${DEST_PATH}" -f "${DEST_FILE}"
+  XZ_OPT=-e9 ionice -c 2 -n 7 nice -n 19 tar -cJhvpnS -C "${SOURCE_PATH}" "${SOURCE_DIR}" -C "${DEST_PATH}" -f "${DEST_FILE}"
 else
   echo "Usage: $0 /caminho/completo/do/diretorio/a/ser/comprimido /caminho/pro/arquivo-final"
 fi
