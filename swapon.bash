ebc27ac8fee0d90d5e319bf575fc3962c11f5c6e
Iuri Guilherme <iuri@fabricadofuturo.com>
2020-01-17 19:30:46 -0300

    Swapping on

--- swapon.bash
+++ swapon.bash
@@ -3,11 +3,26 @@
 
 SWAP_FILE="/srv/swap/swapfile.swap"
 USER=`whoami`
+SIZE=1048576
+
+if [ ! -z $1]
+then
+  let SIZE=SIZE*$1
+else
+  echo "Defaulting to 1GB (${SIZE} kilobytes). Use $0 8 for 8GB."
+fi
 
 if [ "${USER}" == "root" ]
 then
-	mkswap -c "${SWAP_FILE}"
-	chmod -v 600 "${SWAP_FILE}"
-	swapon "${SWAP_FILE}"
+  if [ ! -f ${SWAP_FILE} ]
+  then
+    mkdir -p $(dirname ${SWAP_FILE})
+    dd if=/dev/zero of="${SWAP_FILE}" bs=1024 count=${SIZE}
+  fi
+  chmod -v 600 "${SWAP_FILE}"
+  mkswap -c "${SWAP_FILE}"
+  swapon -v "${SWAP_FILE}"
+else
+  echo "You are not root. GTFO."
 fi
 
