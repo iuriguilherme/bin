25156e200082b80880c4bfada7c1c1e21f4401e0
Iuri Guilherme <iuri@fabricadofuturo.com>
2020-08-11 17:54:56 -0300

    I'm r00t

--- swapon.bash
+++ swapon.bash
@@ -2,7 +2,7 @@
 ## Ativa swap no servidor
 
 SWAP_FILE_NAME="swapfile.swap"
-SWAP_FILE_PATH="/srv/swap"
+SWAP_FILE_PATH="/srv/swap/"
 USER=`whoami`
 SIZE=1048576
 
@@ -10,10 +10,6 @@ if [ ! -z $1 ]
 then
   let SIZE=SIZE*$1
   echo "Will create a file with ${SIZE} *kibibytes* (${1}GB)"
-  if [ -d $2 ]
-  then
-    SWAP_FILE_PATH="${2}"
-  fi
 else
   echo "Defaulting to 1GB (${SIZE} kibibytes). Use $0 8 for 8GB (1 gibibyte times 8 in powers of 1024)."
 fi
@@ -22,18 +18,14 @@ SWAP_FILE="${SWAP_FILE_PATH}/${SWAP_FILE_NAME}"
 
 echo "Will use ${SWAP_FILE} as swap file."
 
-if [ "${USER}" == "root" ]
+if [ -f "${SWAP_FILE}" ]
 then
-  if [ ! -f "${SWAP_FILE}" ]
-  then
-    mkdir -p "${SWAP_FILE_PATH}"
-    dd if=/dev/zero of="${SWAP_FILE}" bs=1024 count=${SIZE} status=progress
-  fi
-  chmod -v 600 "${SWAP_FILE}"
-  mkswap -c "${SWAP_FILE}"
-  swapon -v "${SWAP_FILE}"
-  free -ltw
-else
-  echo "You are not root. GTFO."
+  sudo swapoff "${SWAP_FILE}"
 fi
+sudo mkdir -p "${SWAP_FILE_PATH}"
+sudo dd if=/dev/zero of="${SWAP_FILE}" bs=1024 count=${SIZE} status=progress
+sudo chmod -v 600 "${SWAP_FILE}"
+sudo mkswap -c "${SWAP_FILE}"
+sudo swapon -v "${SWAP_FILE}"
+free -ltw
 
